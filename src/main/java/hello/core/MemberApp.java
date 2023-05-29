c94136636c4a964551bf71a1ff65820fa3b965a3
Eerang2 <117508537+Eerang2@users.noreply.github.com>
2023-05-29 17:40:05 +0900

    dip 원칙을 지키며 할인률 코드 수정

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -7,7 +7,8 @@ import hello.core.member.MemebrServiceImpl;
 
 public class MemberApp {
     public static void main(String[] args) {
-        MemberService memberService = new MemebrServiceImpl();
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
